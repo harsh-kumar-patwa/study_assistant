7db0bf89af2364efd8121d471a1a71902a1f7a83
Harsh Kumar <135590545+harsh-kumar-patwa@users.noreply.github.com>
2024-09-02 01:16:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BASE_DIR = "data"
 SUBJECTS = ["ML", "DSA", "WebDev"]
 EMBEDDING_DIM = 384
 GENERAL_QUERY_THRESHOLD = 0.1
-GEMINI_API_KEY = "AIzaSyDFeUTdLgADY6A5btRVFnU53Ls2Z2Us0gY"
+GEMINI_API_KEY = "AIzaSyDFeUTdLgADY6A5btRVFnU53Ls2Z2Us0gY" (use your own api key, I have revoked this key)
